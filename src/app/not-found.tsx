de7695eb15718803aa28f05388821d6f24b7b03a
Hament <hament@sectaur.org>
2024-03-12 13:11:45 +1300

    not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex">
+      <h1 className="flex justify-center items-center w-full bg-red-400 h-[100dvh] text-4xl">
+        Not found page - No Bro - Nada here !
+      </h1>
+    </div>
+  );
+}
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default function Review({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  if (parseInt(params.reviewId) > 100) {
+    return notFound();
+  }
   return (
     <div className="flex flex-col ">
       <div className="flex h-[100dvh] bg-purple-400 text-2xl justify-center items-center">
