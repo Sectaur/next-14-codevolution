389275209deb37b693eed86adaaab6644b9bede9
Hament <hament@sectaur.org>
2024-03-13 06:37:45 +1300

    router catch all

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,10 +1,16 @@
 export default function Docs({ params }: { params: { slug: string[] } }) {
-  if (params.slug?.length >= 1) {
+  if (params.slug?.length) {
     return (
       <div className="flex flex-col">
-        <h1 className="bg-red-400 h-[100dvh]"> Docs Param {params.slug[0]}</h1>
+        {params.slug.map((slug, index) => {
+          return (
+            <h1 key={index} className="bg-blue-400 h-[10dvh]">
+              {slug}
+            </h1>
+          );
+        })}
       </div>
     );
   }
-  return <h1 className="bg-yellow-400 h-[100dvh]">Docs Catch All</h1>;
+  return <h1 className="bg-yellow-400 h-[10dvh]">Docs Catch All</h1>;
 }
